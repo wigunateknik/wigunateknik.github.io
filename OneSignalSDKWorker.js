682b9c806972994a74f18eb47146d5aa9a030352
wigunateknik <tokowigunateknik@gmail.com>
2025-11-12 15:20:10 +0800

    Delete OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++
@@ -1,9 +0,0 @@
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-
-// 🔧 Tangani klik notifikasi (biar bisa dibuka saat ditekan)
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow('https://wigunateknik.github.io/')
-  );
-});
